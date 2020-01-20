e4f53e6affb46a7dffa061e2acd6b742485933d7
Tirth7545 <tirthraj277@gmail.com>
2020-01-21 02:09:06 +0530

    Major changes and like dislike added

--- lib/add.dart
+++ lib/add.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
+import 'bouncing_button.dart';
+
 class Add extends StatefulWidget {
   @override
   _AddState createState() => _AddState();
@@ -12,6 +14,7 @@ class _AddState extends State<Add> {
   TextEditingController titleController = TextEditingController();
   TextEditingController descriptionController = TextEditingController();
   bool isSwitched = true;
+
   @override
   Widget build(BuildContext context) {
     TextStyle textStyle = Theme.of(context).textTheme.title;
@@ -25,202 +28,180 @@ class _AddState extends State<Add> {
         padding: EdgeInsets.only(top: 15.0, left: 10.0, right: 10.0),
         child: ListView(
           children: <Widget>[
-
             // First element
             ListTile(
               title: DropdownButton(
                 value: _selectedLocation,
                 hint: Text("Please Choose local category                 "),
-                  onChanged: (newValue)
-                {
+                onChanged: (newValue) {
                   setState(() {
-                    _selectedLocation=newValue;
+                    _selectedLocation = newValue;
                   });
                 },
-                  items: _locations.map((location) {
-                    return DropdownMenuItem(
-                      child: new Text(location),
-                      value: location,
-                    );
-                  }).toList(),
-
-
+                items: _locations.map((location) {
+                  return DropdownMenuItem(
+                    child: new Text(location),
+                    value: location,
+                  );
+                }).toList(),
               ),
             ),
 
             // Second Element
-            Padding(
-              padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
-              child: TextField(
-                controller: titleController,
-                style: textStyle,
-                onChanged: (value) {
-                  debugPrint('Something changed in Title Text Field');
-                },
-                decoration: InputDecoration(
-                    labelText: 'Title',
-                    labelStyle: textStyle,
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5.0)
-                    )
-                ),
-              ),
-            ),
+//            Padding(
+//              padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+//              child: TextField(
+//                controller: titleController,
+//                style: textStyle,
+//                onChanged: (value) {
+//                  debugPrint('Something changed in Title Text Field');
+//                },
+//                decoration: InputDecoration(
+//                    labelText: 'Title',
+//                    labelStyle: textStyle,
+//                    border: OutlineInputBorder(
+//                        borderRadius: BorderRadius.circular(5.0)
+//                    )
+//                ),
+//              ),
+//            ),
 
             // Third Element
             Padding(
               padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
-
               child: TextField(
                 maxLines: 8,
                 controller: descriptionController,
-
                 style: textStyle,
                 onChanged: (value) {
                   debugPrint('Something changed in Description Text Field');
                 },
                 decoration: InputDecoration(
-                  hintText: 'Description',
+                    hintText: 'Description',
 //                  contentPadding: const EdgeInsets.symmetric(vertical: 40),
 //                    labelText: 'Description',
                     labelStyle: textStyle,
                     border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5.0)
-                    )
-                ),
+                        borderRadius: BorderRadius.circular(5.0))),
               ),
             ),
-
 
             Row(
               children: <Widget>[
                 Container(
-                  padding: EdgeInsets.only(bottom: 20,top: 15),
-                  child: Text("Do you Want to go Anonymous?",style: TextStyle(color: Colors.blueAccent,fontSize: 15),),
+                  padding: EdgeInsets.only(bottom: 20, top: 15),
+                  child: Text(
+                    "Do you Want to go Anonymous?",
+                    style: TextStyle(color: Colors.blueAccent, fontSize: 15),
+                  ),
                 ),
-                Expanded(
-                  flex: 5,
+                SizedBox(width: 60,),
+                Container(
                   child: Switch(
                     value: isSwitched,
                     onChanged: (value) {
                       setState(() {
-
                         isSwitched = value;
                         print(value);
-                        return Alert(
-                            context: context,
-                            type: AlertType.warning,
-                            title: "Warning",
-                            desc: "Are You Sure To go Publish Your Profile",
-                            buttons: [
-                              DialogButton(
-                                child: Text("Yes"),
-                                onPressed: ()
-                                {
-                                  Navigator.pop(context);
-                                },
-                              ),
-                              DialogButton(
-                                child: Text("No"),
-                                onPressed: ()
-                                {
-                                  isSwitched=true;
-                                  Navigator.pop(context);
-                                },
-                              ),
 
-                            ]
-                        );
                       });
                     },
                     activeTrackColor: Colors.blueAccent,
                     activeColor: Colors.blue,
                   ),
                 )
-
               ],
             ),
-            SizedBox(height: 5,)
-            , // Fourth Element
+            SizedBox(
+              height: 5,
+            ), // Fourth Element
             Padding(
               padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
               child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Expanded(
-                    child: RaisedButton(
-                      color: Theme.of(context).primaryColorDark,
-                      textColor: Theme.of(context).primaryColorLight,
-                      child: Text(
-                        'POST',
-                        textScaleFactor: 1.5,
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          debugPrint("Save button clicked");
-                          if(isSwitched == false)
-                            {
-                          return  Alert(
-                            context: context,
-                            type: AlertType.error,
-                            title: "Warning",
-                            desc: "Are You Sure You Want to Show Your Profile",
-                            buttons: [
-                              DialogButton(
-                                child: Text(
-                                  "Ok",
-                                  style: TextStyle(color: Colors.white, fontSize: 20),
-                                ),
-                                onPressed: () => Navigator.pop(context),
-                                width: 120,
-                              ),
-                              DialogButton(
-                                child: Text(
-                                  "Nope",
-                                  style: TextStyle(color: Colors.white, fontSize: 20),
-
-                                ),
-
-                                onPressed: () => Navigator.pop(context),
-                                width: 120,
-                              )
-                            ],
-                          ).show();}
-                          else{
-                            return  Alert(
-                              context: context,
-                              type: AlertType.error,
-                              title: "Secret",
-                              desc: "Anonymous Posted",
-                              buttons: [
-                                DialogButton(
-                                  child: Text(
-                                    "Ok",
-                                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      RaisedButton(
+                        onPressed: () {
+                          setState(() {
+                            debugPrint("Save button clicked");
+                            if (isSwitched == false) {
+                              return Alert(
+                                context: context,
+                                type: AlertType.error,
+                                title: "Warning",
+                                desc:
+                                    "Are You Sure You Want to Show Your Profile",
+                                buttons: [
+                                  DialogButton(
+                                    child: Text(
+                                      "Ok",
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 20),
+                                    ),
+                                    onPressed: () => Navigator.pop(context),
+                                    width: 120,
                                   ),
-                                  onPressed: () => Navigator.pop(context),
-                                  width: 120,
-                                ),
-
+                                  DialogButton(
+                                    child: Text(
+                                      "Nope",
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 20),
+                                    ),
+                                    onPressed: () => Navigator.pop(context),
+                                    width: 120,
+                                  )
+                                ],
+                              ).show();
+                            } else {
+                              return Alert(
+                                context: context,
+                                type: AlertType.success,
+                                title: "Secret",
+                                desc: "Anonymous Posted",
+                                buttons: [
+                                  DialogButton(
+                                    child: Text(
+                                      "Ok",
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 20),
+                                    ),
+                                    onPressed: () => Navigator.pop(context),
+                                    width: 120,
+                                  ),
+                                ],
+                              ).show();
+                            }
+                          });
+                        },
+                        textColor: Colors.white,
+                        padding: const EdgeInsets.all(0.0),
+                        child: Container(
+                          decoration: const BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: <Color>[
+                                Color(0xFF0D47A1),
+                                Color(0xFF1976D2),
+                                Color(0xFF42A5F5),
                               ],
-                            ).show();
-
-                          }
-                        });
-
-
-
-                      },
-                    ),
+                            ),
+                          ),
+                          padding: const EdgeInsets.all(10.0),
+                          child: const Text('     Post     ',
+                              style: TextStyle(fontSize: 20)),
+                        ),
+                      ),
+                    ],
                   ),
 
-                  Container(width: 5.0,),
-
-
-
+                  Container(
+                    width: 5.0,
+                  ),
                 ],
               ),
             ),
-
           ],
         ),
       ),
--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:like_button/like_button.dart';
 class Home extends StatefulWidget {
   Home({Key key, this.title}) : super(key: key);
 
@@ -15,59 +15,173 @@ class _HomeState extends State<Home> {
     final topAppBar = AppBar(
       elevation: 0.1,
       backgroundColor: Colors.white,
+      automaticallyImplyLeading: false,
       title: Text(
         "Speak Up",
         style: TextStyle(color: Colors.black),
       ),
       actions: <Widget>[
         IconButton(
-          icon: Icon(Icons.list,color: Colors.black),
+          icon: Icon(Icons.list, color: Colors.black),
           onPressed: () {},
         )
       ],
     );
     final makeListTile = ListTile(
-      title: Text('Three-line ListTile'),
-      subtitle: Text(
-          'A sufficiently long subtitle warrants three lines.sfasjfdklajsdfjlsajflasjfdlajsldflasjdflkjaskdfjlasjdfljasldjflajsdlfkjalsdjflasjlfkasldfjlsajk'),
+      title: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text('Category'),
+      ),
       trailing: Icon(Icons.more_vert),
-    );
+      subtitle: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+                'A sufficiently long subtitle warrants three lines.sfasjfdklajsdfjlsajflasjfdlajsldflasjdflkjaskdfjlasjdfljasldjflajsdlfkjalsdjflasjlfkasldfjlsajk'),
+            SizedBox(
+              height: 12,
+            ),
+            Row(
+              children: <Widget>[
+                Container(
+                  child: LikeButton(
 
-    final makeCard = Card(
+                    circleColor:
+                    CircleColor(start: Color(0xff00ddff), end: Color(0xff0099cc)),
+                    bubblesColor: BubblesColor(
+                      dotPrimaryColor: Color(0xff33b5e5),
+                      dotSecondaryColor: Color(0xff0099cc),
+                    ),
+                    likeBuilder: (bool isLiked) {
+                      return Icon(
+                        Icons.arrow_upward,
+                        color: isLiked ? Colors.deepPurpleAccent : Colors.grey,
 
-      elevation: 8.0,
+                      );
+                    },
+                    likeCount: 121,
+                    countBuilder: (int count, bool isLiked, String text) {
+                      var color = isLiked ? Colors.deepPurpleAccent : Colors.grey;
+                      Widget result;
+                      if (count == 0) {
+                        result = Text(
+                          "love",
+                          style: TextStyle(color: color),
+                        );
+                      } else
+                        result = Text(
+                          text,
+                          style: TextStyle(color: color),
+                        );
+                      return result;
+                    },
+                  ),
+                ),
+                SizedBox(width: 45,),
+                Container(
+                  child: LikeButton(
 
-      margin: new EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-      child: Container
-        (
+                    circleColor:
+                    CircleColor(start: Color(0xff00ddff), end: Color(0xff0099cc)),
+                    bubblesColor: BubblesColor(
+                      dotPrimaryColor: Color(0xff33b5e5),
+                      dotSecondaryColor: Color(0xff0099cc),
+                    ),
+                    likeBuilder: (bool isLiked) {
+                      return Icon(
+                        Icons.arrow_downward,
+                        color: isLiked ? Colors.red : Colors.grey,
 
+                      );
+                    },
+                    likeCount: 5,
+                    countBuilder: (int count, bool isLiked, String text) {
+                      var color = isLiked ? Colors.red : Colors.grey;
+                      Widget result;
+                      if (count == 0) {
+                        result = Text(
+                          "love",
+                          style: TextStyle(color: color),
+                        );
+                      } else
+                        result = Text(
+                          text,
+                          style: TextStyle(color: color),
+                        );
+                      return result;
+                    },
+                  ),
+                )
+              ],
 
-        decoration: BoxDecoration(color: Colors.white,
-          shape: BoxShape.circle,
+            ),
+            Divider(
+              color: Colors.purpleAccent,
+            ),
+            Row(
+              children: <Widget>[
+                Container(
+                  child: Text("2 hours ago"),
+                ),
+                SizedBox(
+                  width: 70,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    child: Text(
+                      "~Anonymous",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                )
+              ],
+            ),
 
+          ],
         ),
-        child: makeListTile,
       ),
     );
+
+    final makeCard = Stack(
+      children: <Widget>[
+        Positioned(
+            child: Card(
+          elevation: 8.0,
+          margin: new EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              shape: BoxShape.circle,
+            ),
+            child: makeListTile,
+          ),
+        )),
+
+        
+
+      ],
+    );
+
     final makeBody = Container(
       decoration: BoxDecoration(
-
           gradient: LinearGradient(
               begin: Alignment.topLeft,
               end: Alignment(1.7, 0.0),
               tileMode: TileMode.repeated,
               colors: [
 //                const Color(0xFFFFFFEE), const Color(0xFF999999)
-        Colors.purple[400],
+            Colors.purple[400],
 //        Colors.purple[400],
-        Colors.purple[300],
+            Colors.purple[300],
 //        Colors.purple[300],
 //        Colors.purple[300],
 //        Colors.purple[200]
 //            Colors.blue[700],
 //            Colors.blue[400],
 //            Colors.blue[300]
-      ])),
+          ])),
       child: ListView.builder(
         scrollDirection: Axis.vertical,
         shrinkWrap: true,
